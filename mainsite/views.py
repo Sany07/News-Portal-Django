b45224bb763a588a962f69e8c424108c353afa2e
Md. Ahasan Habib Sany <46066881+Sany07@users.noreply.github.com>
2020-12-07 14:49:54 +0600

    adding related post

--- mainsite/views.py
+++ mainsite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 
 from news.models import Category, News
+from taggit.models import Tag
 
 class HomeView(TemplateView):
     template_name = 'site/pages/index.html'
@@ -32,18 +33,16 @@ class PostSingleView(DetailView):
     context_object_name = 'news'
     template_name = 'site/pages/single.html'
 
-    # def get_queryset(self):
-    #     return super().get_queryset().filter(is_published='True').order_by('-id')
-    def get_related_post(self):       
-        return super().get_queryset().filter(is_published='True', category = self.object.category.id, tags__name__in = self.object.tags.name).order_by('-id')   
-    
+    def get_related_post_by_category(self):       
+        return super().get_queryset().filter(is_published='True', category = self.object.category.id).exclude(id=self.object.id).order_by('-id')   
+
+    def get_related_post_filter_by_tag(self):
+        for tag in Tag.objects.all():
+            return self.get_related_post_by_category().filter(tags = tag)[:4]
+        
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['related_post'] = self.get_related_post()           
-
-        print(context['related_post'])  
-      
-
+        context['related_posts'] = self.get_related_post_filter_by_tag()           
         return context
     
 
