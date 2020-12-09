d3fa40655ced99a6c071f6058225b1226527ce32
Md. Ahasan Habib Sany <46066881+Sany07@users.noreply.github.com>
2020-12-09 19:18:02 +0600

    updated sidebar context processor for filter  most commented & popular nesws

--- core/settings.py
+++ core/settings.py
@@ -73,8 +73,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 # custom
-                'mainsite.context_processors.categories_context_processor',
-                'mainsite.context_processors.sidebar_context_processor',
+                # 'mainsite.context_processors.categories_context_processor',
+                'mainsite.context_processors.custom_context_processor',
 
             ],
         },
--- mainsite/context_processors.py
+++ mainsite/context_processors.py
@@ -2,14 +2,16 @@ from news.models import News, Category
 from comment.models import Comment
 from django.db.models import Count
 
-def categories_context_processor(request):
-    categories = Category.objects.all()
+# def categories_context_processor(request):
+#     categories = Category.objects.all()
     
-    return {'categories': categories}
+#     return {'categories': categories}
 
-def sidebar_context_processor(request):
+def custom_context_processor(request):
     categories = Category.objects.all()
-    # popular_news = News.objects.filter(category=categories[0])
-    popular_news = Comment.objects.values('post__title').annotate(Count('post__id')).order_by('-post__id__count')
-    print(popular_news)
-    return {'popular_news': popular_news}
+    news_list = News.objects.all()
+    popular_news = news_list.filter(category=categories[0]).annotate(Count('post__id')).order_by('-id')
+    most_commented = news_list.annotate(Count('post__id')).order_by('-post__id__count')
+
+    print(most_commented)
+    return {'categories': categories, 'popular_news':popular_news, 'most_commented': most_commented}
