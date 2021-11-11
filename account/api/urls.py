5e78dd4f223aea57bb5a9228f5370a9d41ceaf0f
Ahasan Habib Sany <sany.baust@gmail.com>
2021-11-11 18:31:35 +0600

    updated

--- account/api/urls.py
+++ account/api/urls.py
@@ -2,11 +2,11 @@
 from django.urls import path, include
 from .views import registration
 from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView,TokenVerifyView)
-
+from .custom_claims import MyTokenObtainPairView
 
 urlpatterns = [
      path("register/", registration.as_view(), name="register"),
-     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
      path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
      path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
